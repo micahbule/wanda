c9eac86ef48e961d3ec1e280fcc2e7c82ec6a8ce
Marco Padillo <marco@cynder.io>
2015-02-14 13:22:15 +0800

    Added Stylus

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var expressPath = require('express-path');
 var path = require('path');
+var stylus = require('stylus');
+var nib = require('nib');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var app = express();
+
+function compile(str, path) {
+    return stylus(str)
+        .set('filename', path)
+        .use(nib());
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +26,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(stylus.middleware({
+    src: __dirname + '/public',
+    compile: compile
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 expressPath(app, 'routeMap');
